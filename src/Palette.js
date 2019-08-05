79f41b7a25a61ca779ff5568a86b4a9906b5b474
shownola <swasieko@gmail.com>
2019-08-05 10:37:02 -0700

    styled color slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import ColorBox from './ColorBox';
+import 'rc-slider/assets/index.css';
 import './Palette.css';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+
 
 
 class Palette extends Component {
@@ -21,18 +22,31 @@ class Palette extends Component {
     const colorBoxes = colors[level].map(color => (<ColorBox background={color.hex} name={color.name} /> ));
 
     return (
-      <div className='Palette'>
-        <Slider defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel} />
-      {/* Navbar goes here */}
-        <div className="Palette-colors">
-        {colorBoxes}
-        </div>
-        {/* footer goes here */}
-      </div>
+
+        <div className='Palette'>
+          <div className="slider">
+            <Slider defaultValue={level}
+              min={100}
+              max={900}
+              step={100}
+              onAfterChange={this.changeLevel}
+              trackStyle={{ backgroundColor: 'transparent'}}
+              handleStyle={{ backgroundColor: 'green', outline: 'none', border: '2px solid green', boxShadow: 'none', width: 13, height: 13, marginLeft: -7, marginTop: -2 }}
+              activeHandleStyle={{backgroundColor: 'green', outline: 'none', border: '2px solid green', boxShadow: 'none', width: 13, height: 13}}
+              hoverHandleStyle={{backgroundColor: 'green', outline: 'none', border: '2px solid green', boxShadow: 'none', height: 13, width: 13}}
+              railStyle={{ height: 8 }}
+
+              />
+          </div>
+
+          {/* Navbar goes here */}
+            <div className="Palette-colors">
+            {colorBoxes}
+            </div>
+            {/* footer goes here */}
+          </div>
+
+
     );
   }
 }
