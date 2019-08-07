3fcd1373b6b51717b1f60e99dc322373d1a1ff99
shownola <swasieko@gmail.com>
2019-08-06 17:18:24 -0700

    add Navbar and Footer

--- src/Navbar.js
+++ src/Navbar.js
@@ -24,15 +24,15 @@ class Navbar extends Component {
     this.setState({open: false});
   }
   render(){
-    const {level, changeLevel} = this.props;
+    const {level, changeLevel, showingAllColors} = this.props;
     const {format} = this.state;
     return(
       <header className="Navbar">
         <div className="logo">
           <Link to="/">colorpicker</Link>
-
         </div>
-        <div className="slider-container">
+
+        {showingAllColors && (<div className="slider-container">
           <span>Level: {level}</span>
         <div className="slider">
           <Slider defaultValue={level}
@@ -45,10 +45,10 @@ class Navbar extends Component {
             activeHandleStyle={{backgroundColor: 'green', outline: 'none', border: '2px solid green', boxShadow: 'none', width: 13, height: 13}}
             hoverHandleStyle={{backgroundColor: 'green', outline: 'none', border: '2px solid green', boxShadow: 'none', height: 13, width: 13}}
             railStyle={{ height: 8 }}
-
             />
           </div>
         </div>
+      )}
         <div className="select-container">
         <Select value={format} onChange={this.handleFormatChange}>
           <MenuItem value="hex">HEX - #ffffff</MenuItem>
