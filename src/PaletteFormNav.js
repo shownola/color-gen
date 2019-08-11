28623f41d96ce852f00797ad1d2f2771564ca798
shownola <swasieko@gmail.com>
2019-08-11 09:54:38 -0700

    Add dialog modal

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
+import PaletteMetaForm from './PaletteMetaForm';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -48,18 +49,14 @@ class PaletteFormNav extends Component {
     this.state = {newPaletteName: ""};
     this.handleChange = this.handleChange.bind(this);
   }
-  componentDidMount(){
-    ValidatorForm.addValidationRule('isPaletteNameUnique', value => this.props.palettes.every(
-      ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase())
-    );
-  }
+
   handleChange(evt){
     this.setState({
       [evt.target.name]: evt.target.value
     });
   }
   render(){
-    const { classes, open } = this.props;
+    const { classes, open, palettes, handleSubmit } = this.props;
     const {newPaletteName} = this.state;
     return(
       <div className={classes.root}>
@@ -88,18 +85,8 @@ class PaletteFormNav extends Component {
           </Toolbar>
 
           <div className={classes.navBtns}>
-            <ValidatorForm onSubmit={() => this.props.handleSubmit(newPaletteName)}>
-              <TextValidator
-                label="Palette Name" value={this.state.newPaletteName}
-                name="newPaletteName"
-                onChange={this.handleChange}
-                validators={['required', 'isPaletteNameUnique']}
-                errorMessages={['Enter Palette Name', 'Name already used']}
-              />
 
-            <Button variant="contained" color="primary" type='submit'>Save Palette</Button>
-
-          </ValidatorForm>
+          <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit}/>
           <Link to="/">
             <Button variant="contained" color="secondary">Go Back</Button>
           </Link>
