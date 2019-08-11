0b296be51ddf08478574acc49c23fbcf9f60fc42
shownola <swasieko@gmail.com>
2019-08-11 16:40:43 -0700

    Move JSS styles to separate files

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -3,26 +3,9 @@ import Button from '@material-ui/core/Button';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { withStyles } from '@material-ui/core/styles';
 import { ChromePicker } from 'react-color';
+import styles from './styles/ColorPickerFormStyles';
 
 
-const styles = {
-  picker: {
-    width: '100% !important',
-    marginTop: '2rem',
-    // padding: '1rem'
-  },
-  addColor: {
-    width: '100%',
-    padding: '1rem',
-    marginTop: '1rem',
-    fontSize: '2rem',
-    marginLeft: '1rem'
-  },
-  colorNameInput: {
-    width: '100%',
-    height: '70px'
-  }
-};
 
 class ColorPickerForm extends Component {
   constructor(props){
---
+++ src/styles/ColorPickerFormStyles.js
@@ -0,0 +1,20 @@
+const styles = {
+  picker: {
+    width: '100% !important',
+    marginTop: '2rem',
+    // padding: '1rem'
+  },
+  addColor: {
+    width: '100%',
+    padding: '1rem',
+    marginTop: '1rem',
+    fontSize: '2rem',
+    marginLeft: '1rem'
+  },
+  colorNameInput: {
+    width: '100%',
+    height: '70px'
+  }
+};
+
+export default styles;
