eb9585ada9d2ae4c9be9db7fe708c850f0a38034
shownola <swasieko@gmail.com>
2019-08-12 17:23:55 -0700

    Make PaletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,4 @@
+import sizes from './sizes';
 
 export default {
   root: {
@@ -13,7 +14,12 @@ export default {
     alignItems: 'flex-start',
     flexDirection: 'column',
     flexWrap: 'wrap',
-
+    [sizes.down('xs')]: {
+      width: '75%'
+    },
+    [sizes.down('xl')]: {
+      width: '80%'
+    }
   },
   nav: {
     display: 'flex',
@@ -30,6 +36,13 @@ export default {
     width: '100%',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '5%'
+    gridGap: '2.5rem',
+    [sizes.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)'
+    },
+    [sizes.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+      gridGap: '1.5rem'
+    }
   }
 };
