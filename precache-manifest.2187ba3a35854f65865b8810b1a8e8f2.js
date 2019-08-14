d10e9e5a014eb6c33bbd924b528d1cee8c9d0842
shownola <swasieko@gmail.com>
2019-08-14 12:24:35 -0700

    Updates

---
+++ precache-manifest.2187ba3a35854f65865b8810b1a8e8f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd9455722e1086baa2fde02d135a743",
+    "url": "/color-gen/index.html"
+  },
+  {
+    "revision": "ccb35cc88d35d4af01ec",
+    "url": "/color-gen/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "01ff1a7fcd404386cfb3",
+    "url": "/color-gen/static/css/main.64c7fd01.chunk.css"
+  },
+  {
+    "revision": "ccb35cc88d35d4af01ec",
+    "url": "/color-gen/static/js/2.8d451c6e.chunk.js"
+  },
+  {
+    "revision": "01ff1a7fcd404386cfb3",
+    "url": "/color-gen/static/js/main.747a7277.chunk.js"
+  },
+  {
+    "revision": "73b9e1c47c728c15bd9d",
+    "url": "/color-gen/static/js/runtime~main.e0bb0528.js"
+  },
+  {
+    "revision": "a1cf67980c4d3ee140634fd23a56d277",
+    "url": "/color-gen/static/media/bg.a1cf6798.svg"
+  }
+]);
